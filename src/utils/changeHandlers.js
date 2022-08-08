f9ed1dabe38baecb4fade45b9f778b176cdebf66
evnurm <evn.valtteri@windowslive.com>
2022-08-08 23:23:50 +0300

    Fix changeHandler for checkbox group

--- src/utils/changeHandlers.js
+++ src/utils/changeHandlers.js
@@ -1,10 +1,10 @@
 import { INPUT_TYPES } from "../form-input-types";
 
-const addValue = (value, values) => [...values, value];
+const addValue = (value, values) => values ? [...values, value] : [value];
 const removeValue = (value, values) => values.filter(x => x !== value);
 
 const replaceValue = (value) => value;
-const toggleValue = (value, values) => values.includes(value) ? removeValue(value, values) : addValue(value, values);
+const toggleValue = (value, values) => values?.includes(value) ? removeValue(value, values) : addValue(value, values);
 
 export const getChangeHandler = (inputType) =>  {
   switch (inputType) {
