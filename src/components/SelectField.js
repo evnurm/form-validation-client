6e7c47773031e4751e361238d163af82f2308311
evnurm <evn.valtteri@windowslive.com>
2022-08-22 21:11:46 +0300

    Remove debug console.log from SelectField

--- src/components/SelectField.js
+++ src/components/SelectField.js
@@ -4,7 +4,7 @@ function SelectField({ name, constraints, label, onChange, value: selectedValue 
   return (
     <div>
       <label htmlFor={name} style={{display: 'block'}}>{label}</label>
-      <select name={name} id={name} onChange={(event) => {console.log(event.target.value); onChange(event.target.value)}} value={selectedValue}>
+      <select name={name} id={name} onChange={(event) => onChange(event.target.value)} value={selectedValue}>
         <option value="">Choose {name}</option>
         {constraints.values.map(({ label, value }) => 
           <option value={value}>{label}</option>
